2cbf0074e44dfc86536b1c937504bbe3da9eab3c
luiznaiper <luiz_naiper@hotmail.com>
2022-10-13 14:23:12 -0500

    :construction: Add list products

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data';
 
 const Home = () => {
-  return <Layout title="Home">Home page</Layout>;
+  return (
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
