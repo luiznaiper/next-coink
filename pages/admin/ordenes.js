1dcca70750a5f90adbaaa0fa9ba0c89938d4fe71
luiznaiper <luiz_naiper@hotmail.com>
2022-11-30 07:18:20 -0300

    :construction: Create admin Orders Screen

---
+++ pages/admin/ordenes.js
@@ -0,0 +1,120 @@
+import axios from 'axios';
+import Link from 'next/link';
+import React, { useEffect, useReducer } from 'react';
+import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, orders: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+
+const AdminOrderScreen = () => {
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    orders: [],
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/ordenes`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Layout title="Admin Dashboard">
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <ul>
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/ordenes">
+                <a className="font-bold">Órdenes</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/productos">Productos</Link>
+            </li>
+            <li>
+              <Link href="/admin/usuarios">Usuarios</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="overflow-x-auto md:col-span-3">
+          <h4 className="mb-4 text-xl">Administrador de Órdenes</h4>
+          {loading ? (
+            <div>Cargando...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">ID</th>
+                    <th className="p-5 text-left">USUARIO</th>
+                    <th className="p-5 text-left">FECHA</th>
+                    <th className="p-5 text-left">TOTAL</th>
+                    <th className="p-5 text-left">PAGADO</th>
+                    <th className="p-5 text-left">ENTREGADO</th>
+                    <th className="p-5 text-left">ACCIÓN</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order) => (
+                    <tr key={order._id} className="border-b">
+                      <td className="p-5">{order._id.substring(29, 24)}</td>
+                      <td className="p-5">
+                        {order.user ? order.user.name : 'USUARIO ELIMINADO'}
+                      </td>
+                      <td className="p-5">
+                        {order.createdAt.substring(0, 10)}
+                      </td>
+                      <td className="p-5">${order.totalPrice}</td>
+                      <td className="p-5">
+                        {order.isPaid
+                          ? `${order.paidAt.substring(0, 10)}`
+                          : 'NO PAGADA'}
+                      </td>
+                      <td className="p-5">
+                        {order.isDelivered
+                          ? `${order.deliveredAt.substring(0, 10)}`
+                          : 'NO ENTREGADA'}
+                      </td>
+                      <td className="p-5">
+                        <Link href={`/orden/${order._id}`} passHref>
+                          <a>Detalles</a>
+                        </Link>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+AdminOrderScreen.auth = { adminOnly: true };
+export default AdminOrderScreen;
---
+++ pages/api/admin/ordenes.js
@@ -0,0 +1,20 @@
+import { getSession } from 'next-auth/react';
+import Order from '../../../models/Order';
+import db from '../../../utils/db';
+
+const handler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session || (session && !session.user.isAdmin)) {
+    return res.status(401).send('Inicio de sesión requerido');
+  }
+  if (req.method === 'GET') {
+    await db.connect();
+    const orders = await Order.find({}).populate('user', 'name');
+    await db.disconnect();
+    res.send(orders);
+  } else {
+    return res.status(400).send({ message: 'El método no es permitido' });
+  }
+};
+
+export default handler;
